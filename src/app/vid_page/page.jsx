a2f2a9abf1057d71836db78ec722eb8b3cba1f32
Fikri Zaki Aditama, S.Si. <fikriyomann@gmail.com>
2025-02-01 15:52:31 +0700

    update

---
+++ src/app/vid_page/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+import CustomFooter from "@/components/CustomFooter";
+
+const VidPage = () => {
+  return <>
+    <Navbar/>
+    <div className="w-screen h-screen"></div>
+    <CustomFooter />
+  </>
+};
+
+export default VidPage;
