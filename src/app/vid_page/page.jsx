985d67fa36756212c9db3d7b3680e734f51259a0
Fikri Zaki Aditama, S.Si. <fikriyomann@gmail.com>
2025-02-01 16:34:56 +0700

    update

--- src/app/vid_page/page.jsx
+++ src/app/vid_page/page.jsx
@@ -30,11 +30,11 @@ const VidPage = () => {
     <>
       <Navbar />
       <div className="min-h-screen bg-white">
-        <div className="container mx-auto px-4 py-8">
-          <h1 className="text-center mb-8 text-3xl font-bold text-white">
-            Video Demonstrasi Proses Kimia
+        <div className="container mx-auto px-4 py-8 ">
+          <h1 className="text-center  text-3xl font-bold text-black">
+            Our Videos
           </h1>
-          
+          <div className="h-1 w-36 bg-koreaRed mb-8 m-auto"></div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6">
             {videos.map((video) => (
               <div key={video.id} className="bg-gray-800 rounded-lg shadow-lg p-4">
