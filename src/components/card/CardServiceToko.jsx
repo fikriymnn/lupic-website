15a88dc01c882a81283d099ad9f67b137e7b8f7d
Fikri Zaki Aditama, S.Si. <fikriyomann@gmail.com>
2025-03-15 14:44:37 +0700

    ganti Image ke img

--- src/components/card/CardServiceToko.jsx
+++ src/components/card/CardServiceToko.jsx
@@ -12,7 +12,7 @@ export default function CardServiceToko({ gambar, judul, deskripsi, id,harga }) 
     return (
         <a className="mx-5 mt-4 block rounded-xl hover:bg-gray-200" href={"/services/" + id}>
             <div className="w-[300px] shadow-xl rounded-xl">
-                <Image src={process.env.NEXT_PUBLIC_API_FILE_URL + gambar} alt="foto" width={1000} height={1000} className="w-[270px] h-[270px] rounded-xl m-auto mt-4" />
+                <img src={process.env.NEXT_PUBLIC_API_FILE_URL + gambar} alt="foto" width={1000} height={1000} className="w-[270px] h-[270px] rounded-xl m-auto mt-4" />
                 <div className="px-4 h-24">
                     <h3 className="font-bold text-start text-lg md:mt-2 mt-2 mb-1 text-koreaBlue line-clamp-2">{judul}</h3>
                     <p className="m-auto text-base pb-1 line-clamp-2">{deskripsi}</p>
